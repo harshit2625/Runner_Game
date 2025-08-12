08c423aa7697fd931a26ef476d9df28c5657d3d7
Harshit2625 <harshitlohani52@gmail.com>
2025-08-12 19:58:50 +0530

    Makes some minor changes in the main code Runner.py

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+#include<string>
+
+int main(){
+    std::int16_t grade1=12; 
+    std::int16_t grade2=2;
+    std::string name2="Harshit Lohani";
+    std::string name1="Mahak Joshi";
+ std::cout <<name1 <<" loves " <<name2 <<std::endl << name1 <<" studies in " <<grade1 <<"th " << name2 <<" studies in btech " <<"year" <<grade2 <<std::endl;
+return 0;
+}
